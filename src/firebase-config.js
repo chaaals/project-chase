0539141462276b99fc8db994166629483e33a6fa
chaaals <charlesching0808@gmail.com>
2022-09-25 17:11:36 +0800

    fix: used initializeFirestore on db

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,4 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true, // this line
+  useFetchStreams: false, // and this line
+});
